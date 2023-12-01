291ba798fb1c8c028dc722b87864f64645429b6f
Nandor Biro <dev@nandito.hu>
2023-12-01 15:40:20 +0100

    day1: refactor part 2

--- day1/main2.py
+++ day1/main2.py
@@ -1,63 +1,118 @@
-number_words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-def find_all(a_str, sub):
+"""Number words in order from one to nine."""
+number_word_list = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+]
+
+NumberIndicesDict = dict[str, list[int]]
+
+
+def find_all(a_str: str, sub: str):
+    """
+    Find all occurrences of a substring in a string.
+    :param a_str: string to search in
+    :param sub: substring to search for
+    :return: generator of all occurrences
+    """
     start = 0
     while True:
         start = a_str.find(sub, start)
-        if start == -1: return
+        if start == -1:
+            return
         yield start
-        start += len(sub) 
+        start += len(sub)
 
-def get_numbers(line):
-    matches = dict()
+
+def get_numbers(line: str) -> NumberIndicesDict:
+    """
+    Get the digits and their index in a string.
+    :param line: string to search in
+    :return: dictionary of digits and their indices in an array
+    """
+    matches = {}
     chars = [*line]
-    for idx,c in enumerate(chars):
-        if c.isdigit():
-            if c in matches:
-                matches[c] = [*matches[c], idx]
-            else:
-                matches[c] = [idx]
+    for idx, char in enumerate(chars):
+        if char.isdigit():
+            matches[char] = [*matches[char], idx] if char in matches else [idx]
     return matches
-                
 
 
-def get_number_word(line):
-    matches = dict()
-    for idx,word in enumerate(number_words):
-        word_matches = list(find_all(line,word))
+def get_number_words(line) -> NumberIndicesDict:
+    """
+    Get the number words and their index in a string.
+    :param line: string to search in
+    :return: dictionary of number words and their indices in an array
+    """
+    matches = {}
+    for idx, word in enumerate(number_word_list):
+        word_matches = list(find_all(line, word))
         if len(word_matches):
-            num = idx+1
-            matches[num] = word_matches
+            num = idx + 1
+            matches[str(num)] = word_matches
 
     return matches
 
-def find_smallest_index(dictionary):
+
+def find_smallest_index(dictionary: NumberIndicesDict):
+    """
+    Find the smallest index in a dictionary of "number": [indices].
+    :param dictionary: dictionary of "number": [indices]
+    :return: ["number", index]
+    """
     smallest_idx = 100000
-    res = []
-    for k,v in dictionary.items():
+    number_with_index = []
+    for k, v in dictionary.items():
         for match in v:
             if match < smallest_idx:
                 smallest_idx = match
-                res = [str(k), match]
-    return res
+                number_with_index = [str(k), match]
+    return number_with_index
+
 
 def find_largest_index(dictionary):
+    """
+    Find the largest index in a dictionary of "number": [indices].
+    :param dictionary: dictionary of "number": [indices]
+    :return: ["number", index]
+    """
     largest_idx = -1
-    res = []
-    for k,v in dictionary.items():
+    number_with_index = []
+    for k, v in dictionary.items():
         for match in v:
             if match > largest_idx:
                 largest_idx = match
-                res = [str(k), match]
-    return res
+                number_with_index = [str(k), match]
+    return number_with_index
 
-def get_smaller_num(d1,d2):
+
+def get_smaller_num(d1, d2):
+    """
+    Compare two arrays by their second element and return the smaller ones first element.
+    :param d1: array of [number, index]
+    :param d2: array of [number, index]
+    :return: smaller number
+    """
     if len(d1) == 0:
         return d2[0]
     if len(d2) == 0:
         return d1[0]
     return d1[0] if d1[1] < d2[1] else d2[0]
 
-def get_larger_num(d1,d2):
+
+def get_larger_num(d1, d2):
+    """
+    Compare two arrays by their second element and return the larger ones first element.
+    :param d1: array of [number, index]
+    :param d2: array of [number, index]
+    :return: larger number
+    """
     if len(d1) == 0:
         return d2[0]
     if len(d2) == 0:
@@ -66,8 +121,13 @@ def get_larger_num(d1,d2):
 
 
 def get_string_and_digit_numbers(line):
+    """
+    Get the smallest and largest digit in a string.
+    :param line: string to search in
+    :return: smallest and largest digit
+    """
     numbers = get_numbers(line)
-    number_words = get_number_word(line) 
+    number_words = get_number_words(line)
     smallest_number_word = find_smallest_index(number_words)
     smallest_number = find_smallest_index(numbers)
     smallest_digit = get_smaller_num(smallest_number, smallest_number_word)
@@ -78,13 +138,13 @@ def get_string_and_digit_numbers(line):
     # print(line, smallest_digit+largest_digit)
     # print("numbers:",numbers,"number_words:", number_words)
     # print("smallest_num_w:",smallest_number_word, "smallest_num:", smallest_number, "largest_num_w:",largest_number_word, "largest_num:",largest_number)
-    return int(smallest_digit+largest_digit)
-    
+    return int(smallest_digit + largest_digit)
 
-with open('input') as f:
+
+with open("input") as f:
     row_values = []
-    for line in f:
-        row_value = get_string_and_digit_numbers(line)
+    for file_line in f:
+        row_value = get_string_and_digit_numbers(file_line)
         row_values.append(row_value)
     sums = sum(row_values)
     print(sums)
---
+++ day1/main2_test.py
@@ -0,0 +1,101 @@
+from main2 import (
+    find_all,
+    get_larger_num,
+    get_numbers,
+    get_number_words,
+    find_smallest_index,
+    find_largest_index,
+    get_smaller_num,
+    get_string_and_digit_numbers,
+)
+
+
+def test_find_all():
+    """Test find_all function."""
+    assert list(find_all("two1nine", "two")) == [0]
+    assert list(find_all("two1nine", "nine")) == [4]
+    assert list(find_all("eightwothree", "eight")) == [0]
+    assert list(find_all("eightwothree", "two")) == [4]
+    assert list(find_all("eightwothree", "three")) == [7]
+    assert list(find_all("eightwothree", "one")) == []
+    assert list(find_all("4nineeightseven2", "nine")) == [1]
+    assert list(find_all("7pqrstsixteensixsix", "six")) == [6, 13, 16]
+
+
+def test_get_numbers():
+    """Test get_numbers function."""
+    assert get_numbers("two1nine") == {"1": [3]}
+    assert get_numbers("eightwothree") == {}
+    assert get_numbers("abcone2threexyz") == {"2": [6]}
+    assert get_numbers("xtwone3four") == {"3": [6]}
+    assert get_numbers("4nineeightseven2") == {"4": [0], "2": [15]}
+    assert get_numbers("zoneight234") == {"2": [8], "3": [9], "4": [10]}
+    assert get_numbers("7pqrstsixteen") == {"7": [0]}
+    assert get_numbers("7pqrst7sixteen7") == {"7": [0, 6, 14]}
+
+
+def test_get_number_words():
+    """Test get_number_words function."""
+    assert get_number_words("two1nine") == {"2": [0], "9": [4]}
+    assert get_number_words("eightwothree") == {"8": [0], "2": [4], "3": [7]}
+    assert get_number_words("abcone2threexyz") == {"1": [3], "3": [7]}
+    assert get_number_words("xtwone3four") == {"2": [1], "1": [3], "4": [7]}
+    assert get_number_words("4nineeightseven2") == {"9": [1], "8": [5], "7": [10]}
+    assert get_number_words("zoneight234") == {"1": [1], "8": [3]}
+    assert get_number_words("7pqrstsixteen") == {"6": [6]}
+    assert get_number_words("7pqrst7sixteen7sixsix") == {"6": [7, 15, 18]}
+
+
+def test_find_smallest_index():
+    """Test find_smallest_index function."""
+    assert find_smallest_index({"2": [0], "9": [4]}) == ["2", 0]
+    assert find_smallest_index({"8": [0], "2": [4], "3": [7]}) == ["8", 0]
+    assert find_smallest_index({"1": [3], "3": [7]}) == ["1", 3]
+    assert find_smallest_index({"2": [1], "1": [3], "4": [7]}) == ["2", 1]
+    assert find_smallest_index({"9": [1], "8": [5], "7": [10]}) == ["9", 1]
+    assert find_smallest_index({"1": [1], "8": [3]}) == ["1", 1]
+    assert find_smallest_index({"6": [6]}) == ["6", 6]
+    assert find_smallest_index({"6": [7, 15, 18]}) == ["6", 7]
+
+
+def test_find_largest_index():
+    """Test find_largest_index function."""
+    assert find_largest_index({"2": [0], "9": [4]}) == ["9", 4]
+    assert find_largest_index({"8": [0], "2": [4], "3": [7]}) == ["3", 7]
+    assert find_largest_index({"1": [3], "3": [7]}) == ["3", 7]
+    assert find_largest_index({"2": [1], "1": [3], "4": [7]}) == ["4", 7]
+    assert find_largest_index({"9": [1], "8": [5], "7": [10]}) == ["7", 10]
+    assert find_largest_index({"1": [1], "8": [3]}) == ["8", 3]
+    assert find_largest_index({"6": [6]}) == ["6", 6]
+    assert find_largest_index({"6": [7, 15, 18]}) == ["6", 18]
+
+
+def test_get_smaller_num():
+    """Test get_smaller_num function."""
+    assert get_smaller_num(["2", 0], ["9", 4]) == "2"
+    assert get_smaller_num(["9", 1], ["7", 10]) == "9"
+    assert get_smaller_num(["7", 10], ["9", 1]) == "9"
+    assert get_smaller_num(["8", 3], ["1", 1]) == "1"
+    assert get_smaller_num([], ["9", 4]) == "9"
+    assert get_smaller_num(["9", 4], []) == "9"
+
+
+def test_get_larger_num():
+    """Test get_larger_num function."""
+    assert get_larger_num(["2", 0], ["9", 4]) == "9"
+    assert get_larger_num(["9", 1], ["7", 10]) == "7"
+    assert get_larger_num(["7", 10], ["9", 1]) == "7"
+    assert get_larger_num(["8", 3], ["1", 1]) == "8"
+    assert get_larger_num([], ["9", 4]) == "9"
+    assert get_larger_num(["9", 4], []) == "9"
+
+
+def test_get_string_and_digit_numbers():
+    """Test get_string_and_digit_numbers function."""
+    assert get_string_and_digit_numbers("two1nine") == 29
+    assert get_string_and_digit_numbers("eightwothree") == 83
+    assert get_string_and_digit_numbers("abcone2threexyz") == 13
+    assert get_string_and_digit_numbers("xtwone3four") == 24
+    assert get_string_and_digit_numbers("4nineeightseven2") == 42
+    assert get_string_and_digit_numbers("zoneight234") == 14
+    assert get_string_and_digit_numbers("7pqrstsixteen") == 76
