327fa42c64a5d59c843afb3b7ebec39787f764a8
Nandor Biro <dev@nandito.hu>
2023-12-01 14:47:19 +0100

    day1: refactor part 1

--- day1/main.py
+++ day1/main.py
@@ -1,23 +1,29 @@
-def get_numbers(line):
+def get_numbers(line: str):
+    """
+    Get the sum of the first and last digit of a string of numbers.
+    :param line: string of numbers
+    :return: concatenation of the first and last digit
+    """
+
     chars = [*line]
     first_digit = None
     cache_digit = None
-    for c in chars:
-        if c.isdigit():
+    for char in chars:
+        if char.isdigit():
             if first_digit is None:
-                first_digit = c
+                first_digit = char
             else:
-                cache_digit = c
+                cache_digit = char
 
     last_digit = cache_digit if cache_digit is not None else first_digit
 
     return first_digit + last_digit
 
 
-with open('input') as f:
+with open("input") as f:
     row_values = []
-    for line in f:
-        row_value = get_numbers(line)
+    for file_line in f:
+        row_value = get_numbers(file_line)
         row_values.append(row_value)
     sums = sum([int(n) for n in row_values])
     print(sums)
---
+++ day1/main_test.py
@@ -0,0 +1,16 @@
+from main import get_numbers
+
+
+def test_get_numbers():
+    """Test get_numbers function."""
+    assert get_numbers("1234") == "14"
+    assert get_numbers("91212129") == "99"
+    assert get_numbers("1one") == "11"
+    assert get_numbers("one1") == "11"
+    assert get_numbers("one1one") == "11"
+    assert get_numbers("one2one1") == "21"
+    assert get_numbers("one2one1one") == "21"
+    assert get_numbers("1abc2") == "12"
+    assert get_numbers("pqr3stu8vwx") == "38"
+    assert get_numbers("a1b2c3d4e5f") == "15"
+    assert get_numbers("treb7uchet") == "77"
