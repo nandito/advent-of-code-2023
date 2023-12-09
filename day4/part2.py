a3dafab16de00de1ab1915e09934670925aba862
Nandor Biro <dev@nandito.hu>
2023-12-09 21:15:11 +0100

    day4: solve part 2

--- day4/part2.py
+++ day4/part2.py
@@ -1,37 +1,35 @@
 import re
+
 import numpy as np
 
-with open("sample_input") as f:
+with open("input") as f:
     lines = f.readlines()
-    pattern = r"Card.*(\d+):\s*(\d+(?:\s*\d*)*)\s*\|\s*(\d+(?:\s*\d*)*)"
-    points = []
-    scratchcards = np.array([])
+    pattern = r"Card\s*(\d+):\s*(\d+(?:\s*\d*)*)\s*\|\s*(\d+(?:\s*\d*)*)"
+    occurrence_map = {}
     for line in lines:
         match = re.match(pattern, line)
         if match:
             card_number = int(match.group(1))
             winning_numbers = list(map(int, match.group(2).split()))
             my_numbers = list(map(int, match.group(3).split()))
-            scratchcards = np.append(scratchcards, card_number)
-            # print("scratchcards", scratchcards)
+            # print("card_number", card_number, "winning_numbers", winning_numbers, "my_numbers", my_numbers)
+            occurrence_map[card_number] = occurrence_map.get(card_number, 0) + 1
 
             all_numbers = np.concatenate((winning_numbers, my_numbers))
             unique = np.unique(all_numbers)
             winning_copy_count = len(all_numbers) - len(unique)
-            # print("card_number:", card_number, "winning_copy_count", winning_copy_count)
 
-            # count how many times we have card_number in scratchcards and run in x times
-            occurrences = np.count_nonzero(scratchcards == card_number)
-            # print("occurrences", occurrences)
-            for o in range(0, occurrences):
-                for cn in range(1, winning_copy_count + 1):
-                    next_card_number = card_number + cn
-                    # print("---adding next_card_number", next_card_number)
-                    scratchcards = np.append(scratchcards, next_card_number)
+            occurrences = occurrence_map.get(card_number, 0)
+            # print("\noccurrences", occurrences)
+            for cn in range(1, winning_copy_count + 1):
+                next_card_number = card_number + cn
+                # print("---adding next_card_number", next_card_number)
+                occurrence_map[next_card_number] = (
+                    occurrence_map.get(next_card_number, 0) + occurrences
+                )
 
         else:
             print("No match found.", line)
 
-    # print("scratchcards", np.sort(scratchcards))
-    print(len(scratchcards))
-    # print(sum(points))
+    # print("occurrence_map", occurrence_map)
+    print("total scorecards", sum(occurrence_map.values()))
