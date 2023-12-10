e013a1327016cdd304637aad56b8a624604141d6
Nandor Biro <dev@nandito.hu>
2023-12-10 19:01:51 +0100

    day10: part 1 works with recursion on sample input

--- day10/part1.py
+++ day10/part1.py
@@ -22,38 +22,58 @@ DIRECTIONS = {
 # If tile is S again, stop and print the directions
 
 
-def find_possible_direction(pos, maze, steps=0):
+def find_possible_direction(prev_pos, pos, maze, steps=0):
     tile = maze[pos[0], pos[1]]
-    print(f"\npos {pos}, tile {tile}, steps {steps}")
+    # print(f"\npos {pos}, tile {tile}, steps {steps}, prev_pos {prev_pos}")
     if tile == "S" and steps > 0:
         return steps
     options = DIRECTIONS[tile]
     next_options = []
+    next_pos = None
     for option in options:
+        # print("!!!! option", option)
         next_row = pos[0] + option[0]
         next_col = pos[1] + option[1]
-        next_pos_tile = maze[next_row, next_col]
-        next_pos_option = DIRECTIONS[next_pos_tile]
-        if next_pos_tile == ".":
+        if np.array_equal(prev_pos, [next_row, next_col]):
+            # print("Skipping option going back", option)
             continue
+        if next_row < 0 or next_col < 0:
+            # print("Skipping option going out of bounds", option)
+            continue
+        # print("next_row,next_col", next_row, next_col)
+        next_pos_tile = maze[next_row, next_col]
+        # print("next_pos_tile", next_pos_tile)
+        next_pos_option = DIRECTIONS[next_pos_tile]
+        # print("next_pos_option before", next_pos_option)
+        if next_pos_tile == ".":
+            # print("Skipping option going to a ground", option)
+            continue
+        prelen = len(next_pos_option)
         next_pos_option = np.array([subarr for subarr in next_pos_option if not np.array_equal(subarr, -option)])
+        prolen = len(next_pos_option)
+        # print("next_pos_option after", next_pos_option)
         if len(next_pos_option) == 0:
+            # print("Skipping option that goes nowhere", option)
+            continue
+        if prelen == prolen:
+            # print("Skipping option that doesn't connect", option)
             continue
 
-        next_option = DIRECTIONS[next_pos_tile]
-        next_options.append(next_option)
-    # print("\n\noptions", options)
-    print("next_options", next_options[-1])
-    # TODO: next_options should be a coord in the maze, not a direction
-    next_pos = pos + next_options[-1][0]
-    print("next_pos", next_pos)
-    return find_possible_direction(next_pos, maze, steps + 1)
-    # return find_possible_direction(next_options[0], maze, steps + 1)
+        # print("next_pos_option", next_pos_option)
+        # print("pos", pos)
+        next_pos = [next_row, next_col]
+        # print("current next_pos", next_pos)
+
+        # next_options.append(next_option)
+    # print("next_options", next_options[-1])
+    # print("next_pos", next_pos)
+    return find_possible_direction(pos, next_pos, maze, steps + 1)
 
 def solve_part1(lines):
     maze = np.array([list(line.strip()) for line in lines])
     print(maze)
     # Find "S" position
     s_pos = np.argwhere(maze == "S")[0]
-    possible_dir = find_possible_direction(s_pos, maze)
+    possible_dir = find_possible_direction(s_pos, s_pos, maze)
+    print(int(possible_dir / 2))
     pass
