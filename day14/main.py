fad4c302042f9e47ae2443b0019910eeab27184e
Nandor Biro <dev@nandito.hu>
2023-12-15 11:08:40 +0100

    day14: refactor main.py

--- day14/main.py
+++ day14/main.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 from part1 import solve_part1
 from part2 import solve_part2
@@ -26,11 +27,14 @@ parser.add_argument(
 args = parser.parse_args()
 
 with open(args.input_file) as f:
+    start = time.time()
     if args.part == "1":
-        solve_part1(f)
+        result = solve_part1(f)
     elif args.part == "2":
         result = solve_part2(f)
     else:
         print("Invalid part specified")
         exit(1)
+    end = time.time()
     print(f"Part {args.part}: {result}")
+    print("It took {:.2f} s to compute".format(end - start))
--- day14/part1.py
+++ day14/part1.py
@@ -15,12 +15,9 @@ def tilt(row):
     return new_row
 
 
-def solve_part1(lines):
-    rows = np.array([[*line.strip()] for line in lines])
-    tilted_rows = np.array(list(map(tilt, rows.T))).T
-    # print(tilted_rows)
+def sum_up(rows):
     counts = np.array(
-        list(map(lambda row: np.count_nonzero(np.array(row) == "O"), tilted_rows))
+        list(map(lambda row: np.count_nonzero(np.array(row) == "O"), rows))
     )
     reversed_counts = np.flip(counts)
     # print(counts)
@@ -28,5 +25,13 @@ def solve_part1(lines):
     total = sum(
         list(map(lambda idx: (idx + 1) * reversed_counts[idx], range(len(counts))))
     )
+    return total
 
-    print(f"Part 1: {total}")
+
+def solve_part1(lines):
+    rows = np.array([[*line.strip()] for line in lines])
+    tilted_rows = np.array(list(map(tilt, rows.T))).T
+    # print(tilted_rows)
+    total = sum_up(tilted_rows)
+    return total
+
