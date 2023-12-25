3d9638dba2c1d6b9f9b0eaead79716f28ba2a6e8
Nandor Biro <dev@nandito.hu>
2023-12-25 10:58:21 +0100

    day24: solve part 1

--- day24/part1.py
+++ day24/part1.py
@@ -2,8 +2,8 @@ from itertools import combinations
 
 import shapely
 
-TEST_AREA = [7, 27]
-# TEST_AREA = [200000000000000, 400000000000000]
+# TEST_AREA = [7, 27]
+TEST_AREA = [200000000000000, 400000000000000]
 
 
 def get_intersection_point(stone1, stone2):
@@ -13,37 +13,15 @@ def get_intersection_point(stone1, stone2):
     vel2 = stone2[1]
     # print(pos1, pos2)
 
-    time_elapsed = 1
-    has_intersection = False
+    pos1f = [pos1[0] + vel1[0] * TEST_AREA[1], pos1[1] + vel1[1] * TEST_AREA[1]]
+    pos2f = [pos2[0] + vel2[0] * TEST_AREA[1], pos2[1] + vel2[1] * TEST_AREA[1]]
+
+    line1 = shapely.LineString([pos1, pos1f])
+    line2 = shapely.LineString([pos2, pos2f])
     intersection = None
-    while not has_intersection:
-        pos1f = [pos1[0] + vel1[0] * time_elapsed, pos1[1] + vel1[1] * time_elapsed]
-        pos2f = [pos2[0] + vel2[0] * time_elapsed, pos2[1] + vel2[1] * time_elapsed]
-
-        # :shrug:
-        if (
-            pos1[0] < 0
-            or pos1[1] < 0
-            or pos1f[0] < 0
-            or pos1f[1] < 0
-            or pos2[0] < 0
-            or pos2[1] < 0
-            or pos2f[0] < 0
-            or pos2f[1] < 0
-        ):
-            break
-
-        line1 = shapely.LineString([pos1, pos1f])
-        line2 = shapely.LineString([pos2, pos2f])
-        # print(line1, line2)
-        if shapely.intersects(line1, line2):
-            has_intersection = True
-            intersection = shapely.intersection(line1, line2)
-            # print("has_intersection after time_elapsed", time_elapsed)
-        # if time_elapsed > 100:
-        # break
-
-        time_elapsed += 1
+    # print(line1, line2)
+    if shapely.intersects(line1, line2):
+        intersection = shapely.intersection(line1, line2)
 
     # print(intersection)
     return intersection
@@ -71,8 +49,6 @@ def solve_part1(lines):
         hailstone_states.append(line_parts)
 
     pairs = list(combinations(hailstone_states, 2))
-
-    # get_intersection_point(pairs[0][0], pairs[0][1])
 
     intersections = list(
         map(lambda pair: get_intersection_point(pair[0], pair[1]), pairs)
